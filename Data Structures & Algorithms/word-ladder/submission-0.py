d0a4a1dd7e9ba175d829df32177217e3596cd3b1
Soham Wadhwa <soham.wadhwa2006@gmail.com>
2026-04-24 16:23:03 +0530

    Add: word-ladder - submission-0

---
+++ Data Structures & Algorithms/word-ladder/submission-0.py
@@ -0,0 +1,22 @@
+from collections import deque
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        word_list = set(wordList)
+        word_list.discard(beginWord)
+
+        q = deque()
+        q.append((beginWord, 1))
+
+        while q:
+            word, step = q.popleft()
+
+            if word == endWord:
+                return step
+            
+            for i in range(len(word)):
+                for ch in 'abcdefghijklmnopqrstuvwxyz':
+                    new_word = word[:i] + ch + word[i + 1:]
+                    if new_word in word_list:
+                        word_list.remove(new_word)
+                        q.append((new_word, step + 1))
+        return 0
